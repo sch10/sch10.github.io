def6c6be0db732ad016e2d8720b98d3bcb90901a
Santosh <santoshchandra0708@gmail.com>
2018-03-24 00:36:26 -0400

    realte final

---
+++ geometry/Relatable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometry;
+
+/**
+ *
+ * @author Santosh
+ */
+public interface Relatable {
+
+    public int isLargerThan(Relatable obj);
+}
